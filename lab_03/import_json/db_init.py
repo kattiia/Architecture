409a0957b7b263bffdc87aed27733fda386b7015
kattiia <1296757@mail.ru>
2024-04-10 02:33:20 +0300

    third lab v5

--- lab_03/import_json/db_init.py
+++ lab_03/import_json/db_init.py
@@ -20,15 +20,15 @@ users_table = Table(
     extend_existing=True  # Расширяем существующую таблицу, если она уже существует
 )
 
-posts_table = Table(
-    'posts', metadata,
-    Column('id', Integer, primary_key=True),  # Новый столбец id с типом данных serial и первичным ключом
-    Column('user_id', Integer),  # Столбец user_id
-    Column('post_text', String),  # Столбец post_text
-    Column('date_time', DateTime, server_default=func.now()),  # Столбец DateTime
-    Column('media', String),  # Столбец media
-    extend_existing=True  # Расширяем существующую таблицу, если она уже существует
-)
+# posts_table = Table(
+#     'posts', metadata,
+#     Column('id', Integer, primary_key=True),  # Новый столбец id с типом данных serial и первичным ключом
+#     Column('user_id', Integer),  # Столбец user_id
+#     Column('post_text', String),  # Столбец post_text
+#     Column('date_time', DateTime, server_default=func.now()),  # Столбец DateTime
+#     Column('media', String),  # Столбец media
+#     extend_existing=True  # Расширяем существующую таблицу, если она уже существует
+# )
 
 #index = Index('fn', users_table.c.first_name)
 
@@ -39,11 +39,11 @@ metadata.create_all(engine)
 
 # Загружаем данные из JSON
 df1 = pd.read_json(r"import_json/UsersJson.json")
-df2 = pd.read_json(r"import_json/PostsJson.json")
+# df2 = pd.read_json(r"import_json/PostsJson.json")
 
 # Добавляем столбец login
 df1['login'] = df1['email']
 
 # Сохраняем данные в таблицу 'users' в базе данных
 df1.to_sql("users", con=engine, if_exists='append', index=False)
-df2.to_sql("posts", con=engine, if_exists='append', index=False)
+# df2.to_sql("posts", con=engine, if_exists='append', index=False)
